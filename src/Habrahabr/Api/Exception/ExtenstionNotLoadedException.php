c8cba2d1c92473c850fc3ff366f7db7fd21459a3
dotzero <mail@dotzero.ru>
2016-04-13 23:07:03 +0300

    Add Exception PHPDoc

--- src/Habrahabr/Api/Exception/ExtenstionNotLoadedException.php
+++ src/Habrahabr/Api/Exception/ExtenstionNotLoadedException.php
@@ -2,6 +2,19 @@
 
 namespace Habrahabr\Api\Exception;
 
+/**
+ * Class ExtenstionNotLoadedException
+ *
+ * @package Habrahabr
+ * @version 0.0.8
+ * @author thematicmedia <info@tmtm.ru>
+ * @link https://tmtm.ru/
+ * @link https://habrahabr.ru/
+ * @link https://github.com/thematicmedia/habrahabr_api
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 class ExtenstionNotLoadedException extends \Exception
 {
 }
